318b71565d9c2602582536d7d142e0545bffd323
Steven Hessing <steven@byoda.org>
2024-09-30 07:37:10 -0700

    service_worker detects social network from intercepted API call

--- dist/jwt_grabber.js
+++ dist/jwt_grabber.js
@@ -12,14 +12,25 @@ chrome.webRequest.onSendHeaders.addListener(
         console.log('Event triggered!');
 
         var headers = details.requestHeaders;
-        var url = headers.url
+        var url = new URL(details.url)
+        var hostname = url.hostname.toLowerCase();
+        if (hostname.includes('x.com') || hostname.includes('twitter.com')) {
+            console.log('Twitter: ' + url.hostname);
+        } else if (hostname.includes('youtube.com')) {
+            console.log('YouTube: ' + url.hostname);
+        }
+
         for (var i = 0, l = headers.length; i < l; ++i) {
-            console.log('Header: ' + headers[i].name + ' --> ' + headers[i].value);
             if (headers[i].name === 'authorization') {
                 console.log('JWT Token: ' + headers[i].value);
-                // Send JWT Token to background.js
-                // chrome.runtime.sendMessage({jwt: headers[i].value});
             }
+            else if (headers[i].name === 'x-csrf-token') {
+                console.log('CSRF Token: ' + headers[i].value);
+            }
+            // console.log('Header: ' + headers[i].name + ' --> ' + headers[i].value);
+
+            // Send JWT Token to background.js
+                // chrome.runtime.sendMessage({jwt: headers[i].value});
         }
     },
     requestFilter,
